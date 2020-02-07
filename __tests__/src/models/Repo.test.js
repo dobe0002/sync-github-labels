375a95de36437b7026b9ddda25642c62adc3c227
Kim Doberstein <dobe0002@umn.edu>
2020-02-07 13:05:25 -0600

    Add labels to Repo model

--- __tests__/src/models/Repo.test.js
+++ __tests__/src/models/Repo.test.js
@@ -1,9 +1,58 @@
 import Repo from '../../../src/models/Repo';
+import Label from '../../../src/models/Label';
 
 describe('Repo Model Tests', () => {
   let repo = {};
+  const repoLabels = [
+    new Label({
+      name: 'sameLabel',
+      color: 'ffffff',
+      description: 'this label is the same between repo and master'
+    }),
+    new Label({
+      name: 'orphanLabel',
+      color: 'eeeeee',
+      description: 'this label exists only in the repo'
+    }),
+    new Label({
+      name: 'changedColor',
+      color: 'bbbbbb',
+      description: 'this label has a different color'
+    }),
+    new Label({
+      name: 'changedDescription',
+      color: '999999',
+      description: 'this label has a different description'
+    })
+  ];
+
+  const masterLabels = [
+    new Label({
+      name: 'sameLabel',
+      color: 'ffffff',
+      description: 'this label is the same between repo and master'
+    }),
+    new Label({
+      name: 'newLabel',
+      color: 'cccccc',
+      description: 'this label is only in the master list'
+    }),
+    new Label({
+      name: 'changedColor',
+      color: 'aaaaaa',
+      description: 'this label has a different color'
+    }),
+    new Label({
+      name: 'changedDescription',
+      color: '999999',
+      description: 'this label has a different UPDATED description'
+    })
+  ];
+
   beforeEach(() => {
     repo = new Repo();
+    repo.labels = repoLabels;
+    repo.masterLabels = masterLabels;
   });
   test('Is instance of Repo', () => {
     expect(repo).toBeInstanceOf(Repo);
@@ -19,7 +68,6 @@ describe('Repo Model Tests', () => {
     expect(repo.owner).toEqual('myOwner');
     expect(repo.name).toEqual('myName');
     expect(repo.fullName).toEqual('myOwner/myName');
-    // expect(repo.labels).toEqual(['myLabels']);
   });
   test('Set full name', () => {
     expect(repo.owner).toEqual('');
@@ -27,5 +75,38 @@ describe('Repo Model Tests', () => {
     repo.fullName = 'myOwner/myName';
     expect(repo.owner).toEqual('myOwner');
     expect(repo.name).toEqual('myName');
+  });
+
+  test('Labels to add', () => {
+    expect(repo.labelsToAdd).toEqual([
+      new Label({
+        name: 'newLabel',
+        color: 'cccccc',
+        description: 'this label is only in the master list'
+      })
+    ]);
+  });
+  test('Labels to delete', () => {
+    expect(repo.labelsToRemove).toEqual([
+      new Label({
+        name: 'orphanLabel',
+        color: 'eeeeee',
+        description: 'this label exists only in the repo'
+      })
+    ]);
+  });
+  test('Labels to edit', () => {
+    expect(repo.labelsToEdit).toEqual([
+      new Label({
+        name: 'changedColor',
+        color: 'aaaaaa',
+        description: 'this label has a different color'
+      }),
+      new Label({
+        name: 'changedDescription',
+        color: '999999',
+        description: 'this label has a different UPDATED description'
+      })
+    ]);
   });
 });
--- src/models/Repo.js
+++ src/models/Repo.js
@@ -4,6 +4,28 @@ class Repo {
   constructor(obj = {}) {
     this.repoOwner = _.get(obj, 'owner.login', '');
     this.repoName = obj.name || '';
+    this.repoLabels = obj.labels || [];
+    this.mastLabels = obj.masterLabels || [];
+    this.labelsAdded = []; // tracks the result of an attempt to add a label
+    this.labelsEdited = []; // tracks the result of an attempt to edit a label
+    this.labelsRemoved = []; // tracks the result of an attempt to remove a label
+  }
+
+  /** Set label status * */
+
+  labelAdded(label, error = false, inuse = false) {
+    // label is a label object
+    this.labelsAdded.push({ label, error, inuse });
+  }
+
+  labelEdited(label, error = false, inuse = false) {
+    // label is a label object
+    this.labelsEdited.push({ label, error, inuse });
+  }
+
+  labelRemoved(label, error = false, inuse = false) {
+    // label is a label object
+    this.labelsRemoved.push({ label, error, inuse });
   }
 
   /** Getters and Setters */
@@ -39,8 +61,8 @@ class Repo {
     this.owner = owner;
     this.name = name;
   }
-  /*
-  get labels() {
+
+  get _labels() {
     return this.repoLabels;
   }
 
@@ -48,7 +70,48 @@ class Repo {
     this.repoLabels = labels;
     return this.repoLabels;
   }
-  */
+
+  set masterLabels(labels) {
+    this.mastLabels = labels;
+    return this.mastLabels;
+  }
+
+  get _masterLabels() {
+    return this.mastLabels;
+  }
+
+  get labelsToAdd() {
+    return _.differenceWith(
+      this.mastLabels,
+      this.repoLabels,
+      (label1, label2) => label1.name === label2.name
+    );
+  }
+
+  get labelsToRemove() {
+    // labels in local but not in master
+    return _.differenceWith(
+      this.repoLabels,
+      this.mastLabels,
+      (label1, label2) => label1.name === label2.name
+    );
+  }
+
+  get labelsToEdit() {
+    // labels in both local and master but have differences
+    const dupLabels = _.intersectionWith(
+      this.mastLabels,
+      this.repoLabels,
+      (label1, label2) => label1.name === label2.name
+    );
+    return _.differenceWith(
+      dupLabels,
+      this.repoLabels,
+      (label1, label2) =>
+        label1.color === label2.color &&
+        label1.description === label2.description
+    );
+  }
 }
 
 module.exports = Repo;
