5878c0fcdae3c56585b06e12a7dd598b764f2f40
Kim Doberstein <dobe0002@umn.edu>
2020-02-07 13:09:17 -0600

    Start to add Sync tests

---
+++ __tests__/src/service/Sync.test.js
@@ -0,0 +1,181 @@
+import labelJson from '../../../__fixtures__/git/repoLabels';
+
+const axios = require('axios');
+const Sync = require('../../../src/service/Sync');
+const SyncOptions = require('../../../src/models/SyncOptions');
+const repoLabels = require('../../../__fixtures__/git/repoLabels');
+const Label = require('../../../src/models/Label');
+const Repo = require('../../../src/models/Repo');
+
+describe('Sync tests', () => {
+  axios.setLabels(repoLabels.default);
+  // See the repoLabels file to determine what is expected
+  // Expected labels are actually Label objects
+  const expectedLabels = [
+    {
+      labelColor: 'aabbcc',
+      labelDescription: 'My newly created label.',
+      labelName: 'My New label'
+    },
+    {
+      labelColor: '778899',
+      labelDescription: 'Really troublesome insect.',
+      labelName: 'Bug :bug:'
+    },
+    {
+      labelColor: 'ddeeff',
+      labelDescription: 'This label only lives in the repos.',
+      labelName: 'repo label'
+    },
+    {
+      labelColor: 'ffeedd',
+      labelDescription: 'This other label only lives in the repos.',
+      labelName: 'other repo label'
+    }
+  ];
+  // need tests for missing requirements
+  test('Sync labels from input file and output file', () => {});
+  test('Sync labels from input file and output list', () => {});
+  test('Sync labels from input repo and output file', () => {});
+  test('Sync labels from input repo and output list', () => {});
+  test('Sync labels  with "sync" flag on', () => {});
+  test('Sync labels  with "sync force" flag on', () => {});
+
+  /* **************************************** */
+  /** TEMP TESTS = DELETE AFTER DEVELOPMENT */
+
+  test('Get Labels from file', done => {
+    const options = new SyncOptions({
+      inputFile: '../../__fixtures__/localLabelFile.json',
+      github: 'myGitHubRepo',
+      token: 'myGitHubToken',
+      outputOrg: 'myLabelOrg'
+    });
+    const expected = [
+      {
+        labelName: 'my label',
+        labelColor: 'f00',
+        labelDescription: 'This is label'
+      },
+      {
+        labelName: 'my other label',
+        labelColor: '0f0',
+        labelDescription: 'This is the other label'
+      }
+    ];
+    const sync = new Sync(options);
+    sync._getLabels(error => {
+      expect(error).toBeNull();
+      expect(sync._labelArray[0]).toBeInstanceOf(Label);
+      expect(sync._labelArray).toEqual(expected);
+      done();
+    });
+
+    done();
+  });
+  test('Get Labels from repo', done => {
+    const options = new SyncOptions({
+      inputRepo: 'myOwner/myLabelRepo',
+      github: 'myGitHubRepo',
+      token: 'myGitHubToken',
+      outputOrg: 'myLabelOrg'
+    });
+
+    const sync = new Sync(options);
+    sync._getLabels(error => {
+      expect(error).toBeNull();
+      expect(sync._labelArray[0]).toBeInstanceOf(Label);
+      expect(sync._labelArray).toEqual(expectedLabels);
+      done();
+    });
+  });
+  /* **************************************** */
+  test('Get repos from file', done => {
+    const options = new SyncOptions({
+      inputRepo: 'myOwner/myLabelRepo',
+      github: 'myGitHubRepo',
+      token: 'myGitHubToken',
+      outputRepoFile: '../../__fixtures__/localRepoList.json'
+    });
+    const expected = [
+      new Repo({ owner: { login: 'myOrgFromFile' }, name: 'Repo1' }),
+      new Repo({ owner: { login: 'myOrgFromFile' }, name: 'Repo2' }),
+      new Repo({ owner: { login: 'myOrgFromFile' }, name: 'Repo3' })
+    ];
+    const sync = new Sync(options);
+    sync._getRepos(error => {
+      expect(error).toBeNull();
+      expect(sync._repoArray).toHaveLength(3);
+      expect(sync._repoArray[0]).toBeInstanceOf(Repo);
+      expect(sync._repoArray).toEqual(expected);
+
+      done();
+    });
+  });
+  test('Get repos from array', done => {
+    const options = new SyncOptions({
+      inputRepo: 'myOwner/myLabelRepo',
+      github: 'myGitHubRepo',
+      token: 'myGitHubToken',
+      outputRepos: ['myOrg1/myRepo1', 'myOrg2/myRepo2']
+    });
+    const expected = [
+      new Repo({ owner: { login: 'myOrg1' }, name: 'myRepo1' }),
+      new Repo({ owner: { login: 'myOrg2' }, name: 'myRepo2' })
+    ];
+    const sync = new Sync(options);
+
+    sync._getRepos(error => {
+      expect(error).toBeNull();
+      expect(sync._repoArray).toHaveLength(2);
+      expect(sync._repoArray).toEqual(expected);
+      expect(sync._repoArray[0]).toBeInstanceOf(Repo);
+      done();
+    });
+  });
+  /* ********************************* */
+  test('Get labels for each repo', done => {
+    axios.setLabels(labelJson);
+    const masterLabels = [
+      new Label({
+        name: 'My New label',
+        color: 'aabbcc',
+        description: 'My newly created label.'
+      }),
+      new Label({
+        name: 'repo label',
+        color: 'aaaaaa',
+        description: 'This label only lives in the repos.'
+      }),
+      new Label({
+        name: 'label only in master',
+        color: 'ffffff',
+        description: 'this is a very new label'
+      })
+    ];
+    const options = new SyncOptions({
+      inputRepo: 'myOwner/myLabelRepo',
+      github: 'myGitHubRepo',
+      token: 'myGitHubToken',
+      outputRepos: ['myOrg1/myRepo1', 'myOrg2/myRepo2']
+    });
+
+    const sync = new Sync(options);
+
+    sync._repoArray = [
+      new Repo({ owner: { login: 'myOrg1' }, name: 'myRepo1' }),
+      new Repo({ owner: { login: 'myOrg2' }, name: 'myRepo2' })
+    ];
+    sync._labelArray = masterLabels;
+
+    sync._getRepoLabels(error => {
+      expect(error).toBeNull();
+      expect(sync._repoArray).toHaveLength(2);
+
+      expect(sync._repoArray[0]).toBeInstanceOf(Repo);
+      expect(sync._repoArray[0]._labels).toHaveLength(4);
+      expect(sync._repoArray[0]._masterLabels).toHaveLength(3);
+      done();
+    });
+  });
+});
--- src/service/Sync.js
+++ src/service/Sync.js
@@ -2,63 +2,155 @@
 
 const async = require('async');
 const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
 const log = require('./log');
 const GetLabels = require('./GetLabels');
 const Repo = require('../models/Repo');
+const SyncLabels = require('./SyncLabels');
 
 class Sync {
   constructor(syncOptions) {
     this.syncOptions = syncOptions;
-    this.labels = []; // array of Label objects
-    this.repos = []; // array of Repo objects
+    this.getLabels = new GetLabels(
+      this.syncOptions.token,
+      this.syncOptions.github
+    );
+    this.syncLabels = new SyncLabels(
+      this.syncOptions.token,
+      this.syncOptions.github
+    );
+
+    this.labels = []; // Master array of Label objects
+    this.repos = []; // Array of Repo objects
+  }
+
+  get _labelArray() {
+    return this.labels;
+  }
+
+  set _labelArray(labels) {
+    this.labels = labels;
+  }
+
+  get _repoArray() {
+    return this.repos;
+  }
+
+  set _repoArray(repoArray) {
+    this.repos = repoArray;
   }
 
   syncLabels() {
     async.waterfall(
       [
-        _.bind(this.hasRequired, this),
-        _.bind(this.getLabels, this),
-        _.bind(this.getRepos, this),
-        _.bind(this.saveLabels, this)
+        _.bind(this._hasRequired, this),
+        _.bind(this._getLabels, this),
+        _.bind(this._getRepos, this),
+        _.bind(this._getRepoLabels, this),
+        _.bind(this._addLabels, this),
+        _.bind(this._editLabels, this),
+        _.bind(this._removeLabels, this)
       ],
       error => {
         if (error !== null) {
           log('Label sync encountered an error and did not complete.', 'error');
         }
+        // Report summary
       }
     );
   }
 
-  hasRequired(cb) {
+  _hasRequired(cb) {
     return this.syncOptions.hasRequired()
       ? cb(null)
       : cb('Missing requirements');
   }
 
-  async getLabels(cb) {
+  _getLabels(cb) {
     if (this.syncOptions.inputFile !== '') {
-      this.labels = GetLabels.fromFile(this.syncOptions.inputFile);
-      // TODO check for errors
-      cb(null);
+      this._labelsFromFile(error => cb(error));
     } else if (this.syncOptions.inputRepo !== '') {
-      const getLabels = new GetLabels(
-        this.syncOptions.github,
-        this.syncOptions.token
-      );
-      // TODO check for errors
-
-      const repo = new Repo();
-      repo.fullName = this.syncOptions.inputRepo;
-      this.labels = await getLabels.fromRepo(repo.owner, repo.name);
-      cb(null);
+      this._labelsFromRepo(response => cb(response));
     } else {
       cb('No source of labels');
     }
   }
 
-  getRepos(cb) {}
+  _labelsFromFile(cb) {
+    this.labels = GetLabels.fromFile(this.syncOptions.inputFile);
+    return cb(null);
+  }
 
-  saveLabels(cb) {}
+  async _labelsFromRepo(cb) {
+    // TODO check for errors
+
+    const repo = new Repo();
+    repo.fullName = this.syncOptions.inputRepo;
+    this.labels = await this.getLabels.fromRepo(repo.owner, repo.name);
+    cb(null);
+  }
+
+  _getRepos(cb) {
+    let repos = [];
+    if (this.syncOptions.outputRepos.length !== 0) {
+      repos = this.syncOptions.outputRepos;
+    } else if (this.syncOptions.outputRepoFile !== '') {
+      // TODO check for errors in getting/reading file
+      repos = JSON.parse(
+        fs.readFileSync(
+          path.join(__dirname, this.syncOptions.outputRepoFile),
+          'utf8'
+        )
+      );
+    } else return cb('No output repos set');
+    this.repos = repos.map(repo => {
+      const repoObj = new Repo();
+      repoObj.fullName = repo;
+      return repoObj;
+    });
+    return cb(null);
+  }
+
+  _getRepoLabels(cb) {
+    const self = this;
+
+    return async.eachOfLimit(
+      this.repos,
+      5,
+      async (_repo, index, repoCB) => {
+        self.repos[index].masterLabels = self.labels;
+        self.repos[index].labels = await self.getLabels.fromRepo(
+          self.repos[index].owner,
+          self.repos[index].name
+        );
+        repoCB(null);
+      },
+      error => {
+        return cb(error);
+      }
+    );
+  }
+
+  _addLabels(cb) {
+    const self = this;
+    return async.eachOfLimit(
+      this.repos,
+      5,
+      async (repo, index, repoCB) => {
+        self.syncLabels.addLabelsToRepo(repo, (error, labelsAdded) => {
+          labelsAdded.forEach(label => {
+            // this just updates the repo with status of label adds
+            self.repos[index].labelAdded(label.label, label.error, label.inuse);
+          });
+          repoCB(error);
+        });
+      },
+      error => {
+        return cb(error);
+      }
+    );
+  }
 }
 
 module.exports = Sync;
