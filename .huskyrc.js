9c47363b0d7df2f3672224a45d527b5a5d27aef4
Kim Doberstein <dobe0002@umn.edu>
2020-02-24 16:34:33 -0600

    Add husky precommit hooks

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,7 +4,7 @@ module.exports = {
   hooks: {
     'pre-commit': tasks([
       // 'npm run lint:all' // Use this if you want to check all files before every commit
-      // "lint-staged" // Use this if you want to only check staged files
+      'lint-staged' // Use this if you want to only check staged files
     ])
   }
 };
