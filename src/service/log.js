40e72c82db739f7f84d0f52da579a5f1abd5f276
Kim Doberstein <dobe0002@umn.edu>
2020-03-06 15:44:38 -0600

    Add label  old name to logging

--- src/service/log.js
+++ src/service/log.js
@@ -55,15 +55,19 @@ const _formatReport = (repo, type) => {
   };
 
   console.log(`+++++ Labels ${methods[type].text} +++++`);
-  _.each(repo[methods[type].method], label => {
+  _.each(repo[methods[type].method], labelObj => {
     const errorText =
-      label.error !== null ? `${_LOGSTARTERS.error} Error syncing: ` : '';
+      labelObj.error !== null ? `${_LOGSTARTERS.error} Error syncing: ` : '';
 
     const inuseText =
-      type === 'remove' && label.inuse === true && label.removed === false
+      type === 'remove' && labelObj.inuse === true && labelObj.removed === false
         ? ` ${_LOGSTARTERS.warn} Not removed because in use `
         : '';
-    console.log(`${errorText}${label.label.name}${inuseText}`);
+    let labelName = labelObj.label.name;
+    if (labelObj.label.newName !== '') {
+      labelName = `${labelObj.label.newName} (${labelObj.label.name})`;
+    }
+    console.log(`${errorText}${labelName}${inuseText}`);
   });
   console.log(' ');
 };
