deb267bfd8060f72e2a65ebbc470d07f41223898
Kim Doberstein <dobe0002@umn.edu>
2020-03-06 11:03:53 -0600

    Update Sync.test.js

--- __tests__/src/service/Sync.test.js
+++ __tests__/src/service/Sync.test.js
@@ -305,46 +305,58 @@ describe('Sync tests', () => {
     });
   });
 
-  test.only('Updated labels to repos', done => {
+  test('Updated labels to repos', done => {
     // aka a label name has changed
     const options = new SyncOptions(
       {
-        inputFile: '../../__fixtures__/localLabelFile.json',
         github: 'myGitHubRepo',
-        token: 'myGitHubToken',
-        outputOrg: 'myLabelOrg'
+        token: 'myGitHubToken'
       },
       'noConfig',
       'noConfig'
     );
-    const labelsEdited = [
-      {
-        label: new Label({
-          name: 'changedColor',
+
+    const demoRepo = new Repo({
+      owner: { login: 'myOrg' },
+      name: 'myRepo',
+      labels: [
+        new Label({
+          name: 'my Label',
           color: 'aaaaaa',
-          description: 'this label has a different color'
-        }),
-        error: null,
-        inuse: false
-      },
-      {
-        label: new Label({
-          name: 'changedDescription',
-          color: '999999',
-          description: 'this label has a different description'
-        }),
-        error: null,
-        inuse: false
-      }
-    ];
+          description: 'a description',
+          umn: true
+        })
+      ],
+      masterLabels: [
+        new Label({
+          name: 'my Label',
+          new_name: 'My NEW NAME',
+          color: 'aaaaaa',
+          description: 'a description',
+          umn: true
+        })
+      ]
+    });
+
+    const expected = {
+      label: new Label({
+        name: 'my Label',
+        new_name: 'My NEW NAME',
+        color: 'aaaaaa',
+        description: 'a description',
+        umn: true
+      }),
+      error: null,
+      inuse: false
+    };
 
     const sync = new Sync(options);
-    sync._repoArray = FixtureRepos.repos();
-    sync._editLabels(error => {
+    sync._repoArray = [demoRepo];
+    sync._updateLabels(error => {
       expect(error).toBeNull();
-      expect(sync._repoArray).toHaveLength(2);
-      expect(sync._repoArray[0].labelsEdited).toHaveLength(2);
-      expect(sync._repoArray[0].labelsEdited).toEqual(labelsEdited);
+      expect(sync._repoArray).toHaveLength(1);
+      expect(sync._repoArray[0].labelsUpdated).toHaveLength(1);
+      expect(sync._repoArray[0].labelsUpdated[0]).toEqual(expected);
       done();
     });
   });
